1f479c18b1fd513e7620d264db2d132e39b78ce0
simita <nsimita7@gmail.com>
2024-03-31 12:26:20 +0600

    add a new file

---
+++ practice1.c
@@ -0,0 +1,15 @@
+
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+    
+    char s1[] = "Suparna";
+    int i = 0;
+    while(s1[i] != '\0'){
+        
+        printf("%c\n",s1[i]);
+        i++;
+    }
+    
+}
